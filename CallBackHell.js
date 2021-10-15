6531a93e06194587160d0d84cce0f5f9b71eec64
Muthulakshmi0222 <89353394+Muthulakshmi0222@users.noreply.github.com>
2021-10-15 20:14:14 +0530

    Add files via upload
    Call Back Hell is code

---
+++ CallBackHell.js
@@ -0,0 +1,31 @@
+document.querySelector(".print").innerText="10"
+setTimeout(() =>{   
+  document.querySelector(".print").innerText="9"
+  setTimeout(() =>{   
+  document.querySelector(".print").innerText="8"
+    setTimeout(() =>{   
+  document.querySelector(".print").innerText="7"
+      setTimeout(() =>{   
+  document.querySelector(".print").innerText="6"
+        setTimeout(() =>{   
+  document.querySelector(".print").innerText="5"
+          setTimeout(() =>{   
+  document.querySelector(".print").innerText="4"
+            setTimeout(() =>{   
+  document.querySelector(".print").innerText="3"
+              setTimeout(() =>{   
+  document.querySelector(".print").innerText="2"
+                setTimeout(() =>{   
+  document.querySelector(".print").innerText="1"
+                  setTimeout(() =>{   
+  document.querySelector(".print").innerText="Happy Independence Day"
+  },1000);
+  },1000);
+  },1000);
+  },1000);
+  },1000);
+  },1000);
+  },1000);
+  },1000);
+  },1000);
+  },1000);
